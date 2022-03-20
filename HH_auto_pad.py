3872e6180d213c0ee64cde2ea4d390ded195d625
guomxin <guomaoxin@foxmail.com>
2022-03-20 17:27:03 +0800

    add script for pad

--- HH_auto_pad.py
+++ HH_auto_pad.py
@@ -2,27 +2,15 @@
 #
 import uiautomator2 as u2
 
-'''
-# for 8 NFTs
-NFT_1_1 = (0.254, 0.211)
-NFT_1_2 = (0.734, 0.211)
-NFT_2_1 = (0.254, 0.522)
-NFT_2_2 = (0.734, 0.522)
-NFT_3_1 = (0.254, 0.832)
-NFT_3_2 = (0.734, 0.832)
-
-REFRESH = (0.5, 0.05)
-'''
-
 # for 4 NFTs
-# scroll to bottom
-NFT_1_1 = (0.254, 0.261)
-NFT_1_2 = (0.734, 0.261)
-NFT_2_1 = (0.254, 0.59)
-NFT_2_2 = (0.734, 0.59)
+# scroll to the bottom
+NFT_1_1 = (0.254, 0.07)
+NFT_1_2 = (0.734, 0.07)
+NFT_2_1 = (0.254, 0.486)
+NFT_2_2 = (0.734, 0.486)
 
 #d = u2.connect()
-d = u2.connect_usb("3404842878006NG") # serial number
+d = u2.connect_usb("WER9X19702003497") # serial number
 
 cnt = 0
 while True:
@@ -34,11 +22,11 @@ while True:
     
     btn_buy.click()
     btn_return.click()
-
+    
     cnt += 1
     if cnt % 100 == 0:
         print(f"****loop {cnt} times.")
-    
+
     '''
     btn_buy = d(resourceId="com.tencent.bamboo:id/btn_buy")
     if btn_buy.get_text() == "已售罄":
