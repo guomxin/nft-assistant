2c0dbe8d69df865ed098628ea140ae7f63b66636
guomxin <guomaoxin@foxmail.com>
2022-04-01 08:56:18 +0800

    add run script

---
+++ TaoPai/run.bat
@@ -0,0 +1,6 @@
+python scan_conflux_angel.py 17163
+python scan_conflux_aprilkaozai.py 4771
+python scan_conflux_atsj.py 5428
+python scan_conflux_chaohu.py 1535
+python scan_conflux_dunhuang.py 6020
+python scan_conflux_odin.py 283
