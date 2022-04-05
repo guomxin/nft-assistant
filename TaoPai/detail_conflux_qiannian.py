168b343877f140fb22e50eafba16c416d21884d0
guomxin <guomaoxin@foxmail.com>
2022-04-05 15:13:56 +0800

    add stat details for TaoPai qiannianshouhu

---
+++ TaoPai/detail_conflux_qiannian.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# ContractName: JYY
+
+from datetime import datetime
+from tpcommon import contract
+
+if __name__ == "__main__":
+
+    dump_file_name = "_details_conflux_qiannian_result_{}.csv".format(
+        datetime.strftime(datetime.now(), '%Y%m%d%H%M')
+    )
+    contract.dump_contract_details(
+        contract.JYY_Contract_Address, 
+        contract.JYY_ABI, 
+        dump_file_name,
+        11001, 11500)
