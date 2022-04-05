11e4311b2fd180297e89ca53c6501547a4ed1c0f
guomxin <guomaoxin@foxmail.com>
2022-04-05 17:29:34 +0800

    1.move idrange to common file;2.add detail info digger for atsj

--- TaoPai/scan_conflux_lt.py
+++ TaoPai/scan_conflux_lt.py
@@ -5,31 +5,11 @@ from selenium import webdriver
 from datetime import datetime
 import sys
 
+from tpcommon import idrange
+
 SCAN_URL = "https://confluxscan.io/address/cfx:aapwjebcay7d6jv02whjrrvkm9egmw5fye09cea6zz?NFTAddress=cfx%3Aacaha2pz5j1g6b0fbv9xazv5umm587xucjcdu83b02&limit=50&skip={}&tab=nft-asset"
 CSS_SELECTOR = "div.sc-8rjegh-0.eTefxZ > div > section.sc-fzoNJl.loPePV > div > div > div > div > div > div > div > div > div > div.sc-1hbozql-2.bnWPJO > div.ant-row > div.ant-col"
 FLAG = "TokenID:"
-
-LT_IdRange2Name = {
-    (8382,8680): '19 野餐',
-    (8083,8381): '18 未来',
-    (7784,8082): '17 梦想',
-    (7485,7783): '16 春天',
-    (7186,7484): '15 我爱吃水果',
-    (6887,7185): '14 亲爱的一家人',
-    (6588,6886): '13 星空中的我们',
-    (6289,6587): '12 呐喊',
-    (5990,6288): '11 海蛞蝓',
-    (5691,5989): '10 人与自然2',
-    (5392,5690): '9 花开',
-    (5093,5391): '8 日出',
-    (4794,5092): '7 梦想起航机',
-    (4495,4793): '6 宇航员登月球',
-    (4196,4494): '5 和蓝天做游戏',
-    (3897,4195): '4 丸子公主',
-    (3598,3896): '3 布老虎',
-    (3299,3597): '2 小陆宇宙探险之回归地球',
-    (3000,3298): '1 虎年大吉'
-}
 
 def return_fig_count(name):
     return 299
@@ -46,7 +26,7 @@ if __name__ == "__main__":
     driver.implicitly_wait(20)
 
     fig_cnt_dict = {}
-    for (_, name) in LT_IdRange2Name.items():
+    for (_, name) in idrange.LT_IdRange2Name.items():
         fig_cnt_dict[name] = 0
     for skip_count in range(0, contract_item_count-NEGLECT_COUNT, 50):
         driver.get(SCAN_URL.format(skip_count+NEGLECT_COUNT))
@@ -59,7 +39,7 @@ if __name__ == "__main__":
             flag_pos = nft_text.find(FLAG)
             if flag_pos != -1:
                 token_id = int(nft_text[flag_pos+len(FLAG):].strip())
-                for (range, name) in LT_IdRange2Name.items():
+                for (range, name) in idrange.items():
                     if (token_id >= range[0]) and (token_id <= range[1]):
                         if name not in fig_cnt_dict:
                             fig_cnt_dict[name] = 0
---
+++ TaoPai/tpcommon/detaildigger.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+def dig_a_nftinfo_from_details(details_file_name, min_tid, max_tid, dump_file_name):
+    owner2tidcnt = {}
+    for line in open(details_file_name):
+        items = line.split(",")
+        owner = items[0].strip()
+        tokenids = [int(i) for i in items[2].split(":")]
+        target_tid_cnt = 0
+        for tid in tokenids:
+            if (tid >= min_tid) and (tid <= max_tid):
+                target_tid_cnt += 1
+        if target_tid_cnt > 0:
+            owner2tidcnt[owner] = target_tid_cnt
+    
+    # sort the info
+    owner_tidcnt_list = []
+    for (owner, tidcnt) in owner2tidcnt.items():
+        owner_tidcnt_list.append((owner, tidcnt))
+    owner_tidcnt_list.sort(key=lambda p: p[1], reverse=True)
+
+    dump_file = open(dump_file_name, "w")
+    for (owner, tidcnt) in owner_tidcnt_list:
+        dump_file.write("{},{}\n".format(owner, tidcnt))
+    dump_file.close()    
