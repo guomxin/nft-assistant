246a1da43357f4194c3f4ba961809809ac195ee9
guomxin <guomaoxin@foxmail.com>
2022-04-10 09:34:25 +0800

    add digging fullset info

--- TaoPai/scripts/dig_detail-trans_atsj.cmd
+++ TaoPai/scripts/dig_detail-trans_atsj.cmd
@@ -3,15 +3,23 @@ rem %1-detail tag, %3-start_date, %4-end_date
 cd ..
 
 rem 1. generate details
-rem python detail_conflux_contract.py atsj %1
+python detail_conflux_contract.py atsj %1
 
 rem 2. dig info for QuanJiaFu, LeiShi, Jin, JiaDeLuoSi
-rem python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 38501 38800 QuanJiaFu %1
-rem python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 301 400 LeiShi %1
-rem python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 201 300 Jin %1
-rem python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 101 200 JiaDeLuoSi %1
+python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 38501 38800 QuanJiaFu %1
+python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 301 400 LeiShi %1
+python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 201 300 Jin %1
+python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 101 200 JiaDeLuoSi %1
 
-rem 3. analyze tranctions for QuanJiaFu, LeiShi, Jin, JiaDeLuoSi
+rem 3. dig info for fullset
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 101 38800 ALL %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 10001 38800 ALL-NOHIDE %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 37001 38500 UR %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 34001 37000 SSR %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 25001 34000 SR %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 10001 25000 R %1
+
+rem 4. analyze tranctions for QuanJiaFu, LeiShi, Jin, JiaDeLuoSi
 python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 38501 38800 QuanJiaFu %1
 python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 301 400 LeiShi %1
 python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 201 300 Jin %1
--- TaoPai/scripts/dig_detail-trans_dunhuang.cmd
+++ TaoPai/scripts/dig_detail-trans_dunhuang.cmd
@@ -5,16 +5,15 @@ cd ..
 rem 1. generate details
 python detail_conflux_contract.py dunhuang %1
 
-rem 2. dig info for gold
+rem 2. dig info for gold & purple
 python diginfo_from_details_conflux.py dunhuang 1 data/_details_conflux_dunhuang_result_%1.csv 37001 38000 GOLD %1
-
-rem 3. dig info for purple
 python diginfo_from_details_conflux.py dunhuang 1 data/_details_conflux_dunhuang_result_%1.csv 38001 40000 PURPLE %1
 
-rem 4. analyze tranctions for gold
-python transaction_conflux_contract.py dunhuang trans/SDQH_transactions_%1.csv %2 %3 37001 38000 GOLD %1
+rem 3. dig full-set info
+python diginfo_from_details_conflux.py dunhuang 2 data/_details_conflux_dunhuang_result_%1.csv 10001 40000 ALL %1
 
-rem 5. analyze tranctions for purple
+rem 4. analyze tranctions for gold & purple
+python transaction_conflux_contract.py dunhuang trans/SDQH_transactions_%1.csv %2 %3 37001 38000 GOLD %1
 python transaction_conflux_contract.py dunhuang trans/SDQH_transactions_%1.csv %2 %3 38001 40000 PURPLE %1
 
 cd scripts
