c1ac01cef004dc8f0de58720d0d2df3724fb0471
guomxin <guomaoxin@foxmail.com>
2022-04-27 20:36:19 +0800

    refine atsj scripts

--- TaoPai/scripts/dig_detail-trans_atsj.cmd
+++ TaoPai/scripts/dig_detail-trans_atsj.cmd
@@ -12,19 +12,14 @@ python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_
 python diginfo_from_details_conflux.py atsj 1 data/_details_conflux_atsj_result_%1.csv 101 200 JiaDeLuoSi %1
 
 rem 3. dig info for fullset
-python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 101 38800 ALL %1
-python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 10001 38800 ALL-NOHIDE %1
-python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 37001 38500 UR %1
-python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 34001 37000 SSR %1
-python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 25001 34000 SR %1
-python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 10001 25000 R %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 101,38800 1 ALL %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 10001,38800 1 ALL-NOHIDE %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 37001,38500 1 UR %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 34001,37000 1 SSR %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 25001,34000 1 SR %1
+python diginfo_from_details_conflux.py atsj 2 data/_details_conflux_atsj_result_%1.csv 10001,25000 1 R %1
 
 rem 4. analyze tranctions for QuanJiaFu, LeiShi, Jin, JiaDeLuoSi
-rem python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 38501 38800 QuanJiaFu %1
-rem python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 301 400 LeiShi %1
-rem python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 201 300 Jin %1
-rem python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 101 200 JiaDeLuoSi %1
-
 python transaction_conflux_contract.py atsj trans/ATSJ_transactions_%1.csv %2 %3 10001,25000;25001,34000;34001,37000;37001,38500;38501,38800;101,200;201,300;301,400 R;SR;SSR;UR;QuanJiaFu;Team-JiaDeLuoSi;Team-Jin;Team-LeiShi %1
 
 cd scripts
