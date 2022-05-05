fe9939223aa52267a9127a615f005717dc983de7
guomxin <guomaoxin@foxmail.com>
2022-05-05 21:52:06 +0800

    加入长歌行合成图以及脚本

--- TaoPai/scripts/dig_detail-trans_changgexing.cmd
+++ TaoPai/scripts/dig_detail-trans_changgexing.cmd
@@ -6,7 +6,7 @@ rem 1. generate details
 python detail_conflux_contract.py changgexing %1
 
 rem 2. dig info for SSR
-python diginfo_from_details_conflux.py changgexing 1 data/_details_conflux_changgexing_result_%1.csv 30001 32400 SSR %1
+rem python diginfo_from_details_conflux.py changgexing 1 data/_details_conflux_changgexing_result_%1.csv 30001 32400 SSR %1
 
 rem 3. dig info for fullset
 rem python diginfo_from_details_conflux.py feitianpiba 2 data/_details_conflux_feitianpiba_result_%1.csv 1,800;801,6800;6801,15800 1;2;3 ALL %1
