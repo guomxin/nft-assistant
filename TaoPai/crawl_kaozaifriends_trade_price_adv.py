5633884c5f3956b465aa08207fc28d2a39d9691d
guomxin <guomaoxin@foxmail.com>
2022-05-06 15:36:44 +0800

    防止内存过大错误，轮询完一次关闭浏览器

--- TaoPai/crawl_kaozaifriends_trade_price_adv.py
+++ TaoPai/crawl_kaozaifriends_trade_price_adv.py
@@ -45,60 +45,63 @@ if __name__ == "__main__":
         sys.exit(1)
     keywords = sys.argv[1]
 
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(20)
-
-    # Set cookie
-    driver.get(SCAN_URL.format(1, keywords))
-    driver.add_cookie({'name':'refreshToken', 'value':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTQxNDIzMjQsImlhdCI6MTY1MTU1MDMyNCwidXNlclVJRCI6eyJ1c2VySWQiOjU1MTh9fQ.ogfeGrbHxcHqG2VRkL1smxghyy-Fz6SEf1oTZg2fHBQ', 'path':'/'})
-    driver.add_cookie({'name':'accessToken', 'value':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTE1NTA5MjQsImlhdCI6MTY1MTU1MDMyNCwidXNlclVJRCI6eyJ1c2VySWQiOjU1MTh9fQ.laDpmZHZ-D-bxvxfmDTce7WU7hCS9gXB7KDi-FnT9CQ', 'path':'/'})
-    driver.add_cookie({'name':'cert', 'value':'1', 'path':'/'})
-
-    # Change view
-    driver.get(SCAN_URL.format(1, keywords))
-    change_view_ele = driver.find_element_by_css_selector(CHANGE_VIEW_SELECTOR)
-    change_view_ele.click()
-    time.sleep(1)
-
-    product_dict = {}
     while True:
-        # 获取页面数
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(20)
+
+        # Set cookie
         driver.get(SCAN_URL.format(1, keywords))
-        page_cnt_ele = driver.find_element_by_css_selector(TOTALPAGE_SELECTOR)
-        page_cnt = int(page_cnt_ele.text.strip())
-        print("Total page count:{}.".format(page_cnt))
+        driver.add_cookie({'name':'refreshToken', 'value':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTQxNDIzMjQsImlhdCI6MTY1MTU1MDMyNCwidXNlclVJRCI6eyJ1c2VySWQiOjU1MTh9fQ.ogfeGrbHxcHqG2VRkL1smxghyy-Fz6SEf1oTZg2fHBQ', 'path':'/'})
+        driver.add_cookie({'name':'accessToken', 'value':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTE1NTA5MjQsImlhdCI6MTY1MTU1MDMyNCwidXNlclVJRCI6eyJ1c2VySWQiOjU1MTh9fQ.laDpmZHZ-D-bxvxfmDTce7WU7hCS9gXB7KDi-FnT9CQ', 'path':'/'})
+        driver.add_cookie({'name':'cert', 'value':'1', 'path':'/'})
 
-        # 逐页扫描页面
-        for p in range(page_cnt):
-            driver.get(SCAN_URL.format(p+1, keywords))
-            products = driver.find_elements_by_css_selector(NFTLIST_SELECTOR)
-            for product in products:
-                key_ele = product.find_element_by_css_selector("a > div.m-2 > div")
-                items = key_ele.text.strip().split("#")
-                price = float(items[0].strip()[1:])
-                token_id = items[1].strip()
-                if token_id not in product_dict:
-                    product_dict[token_id] = []
-                if price not in product_dict[token_id]:
-                    product_dict[token_id].append(price)
-            
-        print("{} products.".format(len(product_dict)))
+        # Change view
+        driver.get(SCAN_URL.format(1, keywords))
+        change_view_ele = driver.find_element_by_css_selector(CHANGE_VIEW_SELECTOR)
+        change_view_ele.click()
+        time.sleep(1)
 
-        # dump file
-        result_file = open("data/_crawl_kaozaifriends_trade_{}.csv".format(
-            datetime.strftime(datetime.now(), '%Y%m%d')
-        ), "w")
-        for (token_id, plist) in product_dict.items():
-            result_file.write("{},{}\n".format(
-                token_id,
-                ";".join([str(p) for p in plist])
-            ))
-        result_file.close()
-        time.sleep(30)
+        product_dict = {}
+        try:
+            # 获取页面数
+            driver.get(SCAN_URL.format(1, keywords))
+            page_cnt_ele = driver.find_element_by_css_selector(TOTALPAGE_SELECTOR)
+            page_cnt = int(page_cnt_ele.text.strip())
+            print("Total page count:{}.".format(page_cnt))
 
+            # 逐页扫描页面
+            for p in range(page_cnt):
+                driver.get(SCAN_URL.format(p+1, keywords))
+                products = driver.find_elements_by_css_selector(NFTLIST_SELECTOR)
+                for product in products:
+                    key_ele = product.find_element_by_css_selector("a > div.m-2 > div")
+                    items = key_ele.text.strip().split("#")
+                    price = float(items[0].strip()[1:])
+                    token_id = items[1].strip()
+                    if token_id not in product_dict:
+                        product_dict[token_id] = []
+                    if price not in product_dict[token_id]:
+                        product_dict[token_id].append(price)
+                time.sleep(1)
+                
+            print("{} products.".format(len(product_dict)))
+
+            # dump file
+            result_file = open("data/_crawl_kaozaifriends_trade_{}.csv".format(
+                datetime.strftime(datetime.now(), '%Y%m%d')
+            ), "w")
+            for (token_id, plist) in product_dict.items():
+                result_file.write("{},{}\n".format(
+                    token_id,
+                    ";".join([str(p) for p in plist])
+                ))
+            result_file.close()
+        except Exception as e:
+            print(e)
+        driver.close()
+
+        time.sleep(60)
         # 判断时间是否超过交易时间
         cur_time = datetime.now()
-        if (cur_time.hour >= 15) and (cur_time.minute >= 1):
+        if (cur_time.hour >= 16) and (cur_time.minute >= 1):
             break
-
-    driver.close()
