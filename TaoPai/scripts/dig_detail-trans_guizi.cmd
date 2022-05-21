447679312ec826173fcf6b7d7d68a4134aa17d08
guomxin <guomaoxin@foxmail.com>
2022-05-21 21:23:16 +0800

    完善柜子的统计脚本

--- TaoPai/scripts/dig_detail-trans_guizi.cmd
+++ TaoPai/scripts/dig_detail-trans_guizi.cmd
@@ -5,16 +5,14 @@ cd ..
 rem 1. generate details
 python detail_conflux_contract.py guizi %1
 
-rem 2. dig info for SR
+rem 2. dig info for SR&N
 python diginfo_from_details_conflux.py guizi 1 data/_details_conflux_guizi_result_%1.csv 1 1000 GUIZI-SR %1
-
-rem 3. dig info for N
 python diginfo_from_details_conflux.py guizi 1 data/_details_conflux_guizi_result_%1.csv 1001 4000 GUIZI-N %1
 
-rem 4. analyze tranctions for SR
-python transaction_conflux_contract.py guizi trans/UXON_transactions_%1.csv %2 %3 1 1000 GUIZI-SR %1
+rem 3. dig count in circulation
+python diginfo_from_details_conflux.py guizi 3 data/_details_conflux_guizi_result_%1.csv %1
 
-rem 5. analyze tranctions for N
-python transaction_conflux_contract.py guizi trans/UXON_transactions_%1.csv %2 %3 1001 4000 GUIZI-N %1
+rem 4. analyze tranctions
+python transaction_conflux_contract.py guizi trans/UXON_transactions_%1.csv %2 %3 1,1000;1001,4000 GUIZI-SR;GUIZI-N %1
 
 cd scripts
