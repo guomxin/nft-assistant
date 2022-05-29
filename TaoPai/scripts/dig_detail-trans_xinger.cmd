5ab19e0d0efc0d0b446d81aa0ea9dcb7589274de
guomxin <guomaoxin@foxmail.com>
2022-05-29 21:20:32 +0800

    星儿的统计脚本

---
+++ TaoPai/scripts/dig_detail-trans_xinger.cmd
@@ -0,0 +1,22 @@
+rem %1-detail tag, %2-start_date, %3-end_date
+
+cd ..
+
+rem 1. generate details
+python detail_conflux_contract.py lt %1
+
+rem 2. dig info
+rem python diginfo_from_details_conflux.py lt 1 data/_details_conflux_lt_result_%1.csv 1 10000  ANY %1
+rem python diginfo_from_details_conflux.py lt 1 data/_details_conflux_lt_result_%1.csv 1 50 SSR %1
+rem python diginfo_from_details_conflux.py lt 1 data/_details_conflux_lt_result_%1.csv 51 500 SR %1
+
+rem 3. dig info for fullset
+python diginfo_from_details_conflux.py lt 2 data/_details_conflux_lt_result_%1.csv 3000,8680 2 LIANGTAO %1
+
+rem 4. dig count in circulation
+python diginfo_from_details_conflux.py lt 3 data/_details_conflux_lt_result_%1.csv %1
+
+rem 5. analyze tranctions
+rem python transaction_conflux_contract.py lt trans/BOOOM_transactions_%1.csv %2 %3 1,50;51,500;501,3000;3001,10000 SSR;SR;R;N %1
+
+cd scripts
