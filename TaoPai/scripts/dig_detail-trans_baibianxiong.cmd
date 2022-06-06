7e5002d480e41908e965316f1ae898202fb39940
guomxin <guomaoxin@foxmail.com>
2022-06-06 08:32:27 +0800

    加入端午限定款“ “

--- TaoPai/scripts/dig_detail-trans_baibianxiong.cmd
+++ TaoPai/scripts/dig_detail-trans_baibianxiong.cmd
@@ -8,6 +8,7 @@ python detail_conflux_contract.py baibianxiong %1
 rem 2. dig info for SSR&SR
 python diginfo_from_details_conflux.py baibianxiong 1 data/_details_conflux_baibianxiong_result_%1.csv 1 10000  ANY %1
 python diginfo_from_details_conflux.py baibianxiong 1 data/_details_conflux_baibianxiong_result_%1.csv 1 50 SSR %1
+python diginfo_from_details_conflux.py baibianxiong 1 data/_details_conflux_baibianxiong_result_%1.csv 10001 10015 SSR-DUANWU %1
 python diginfo_from_details_conflux.py baibianxiong 1 data/_details_conflux_baibianxiong_result_%1.csv 51 500 SR %1
 
 rem 3. dig info for fullset
@@ -18,6 +19,6 @@ rem 4. dig count in circulation
 python diginfo_from_details_conflux.py baibianxiong 3 data/_details_conflux_baibianxiong_result_%1.csv %1
 
 rem 5. analyze tranctions
-python transaction_conflux_contract.py baibianxiong trans/BOOOM_transactions_%1.csv %2 %3 1,50;51,500;501,3000;3001,10000 SSR;SR;R;N %1
+python transaction_conflux_contract.py baibianxiong trans/BOOOM_transactions_%1.csv %2 %3 1,50;51,500;501,3000;3001,10000;10001,10015 SSR;SR;R;N;SSR-DUANWU %1
 
 cd scripts
