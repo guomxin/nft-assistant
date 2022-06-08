e7448a162fb51d79e980e0fa0ec6f70e58639789
guomxin <guomaoxin@foxmail.com>
2022-06-08 08:19:27 +0800

    增加早鸟勋章的id范围

--- TaoPai/scripts/dig_detail-trans_xunzhang.cmd
+++ TaoPai/scripts/dig_detail-trans_xunzhang.cmd
@@ -6,7 +6,7 @@ rem 1. generate details
 python detail_conflux_contract.py xunzhang %1
 
 rem 2. dig info for 
-python diginfo_from_details_conflux.py xunzhang 1 data/_details_conflux_xunzhang_result_%1.csv 60001 66140  ZAONIAO %1
+python diginfo_from_details_conflux.py xunzhang 1 data/_details_conflux_xunzhang_result_%1.csv 60001 70000 ZAONIAO %1
 
 rem 3. dig info for fullset
 rem python diginfo_from_details_conflux.py xunzhang 2 data/_details_conflux_xunzhang_result_%1.csv 3001,10000 18 N18 %1
@@ -16,6 +16,6 @@ rem 4. dig count in circulation
 python diginfo_from_details_conflux.py xunzhang 3 data/_details_conflux_xunzhang_result_%1.csv %1
 
 rem 5. analyze tranctions
-python transaction_conflux_contract.py xunzhang trans/TaopaiNFT_transactions_%1.csv %2 %3 60001,66140 ZAONIAO %1
+python transaction_conflux_contract.py xunzhang trans/TaopaiNFT_transactions_%1.csv %2 %3 60001,70000 ZAONIAO %1
 
 cd scripts
