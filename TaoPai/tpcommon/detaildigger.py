9afdc843d1d442a57811cb6fe490a04b14ccc2a5
guomxin <guomaoxin@foxmail.com>
2022-06-10 12:15:03 +0800

    sys.path中增加顶层包的父目录

--- TaoPai/tpcommon/detaildigger.py
+++ TaoPai/tpcommon/detaildigger.py
@@ -1,4 +1,11 @@
 # coding: utf-8
+import os, sys
+# add python path of tpcommon's parent path to sys.path
+parent_path = os.path.abspath(os.path.join(__file__, *([".."]*2)))
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+
+
 from tpcommon import idrange
 
 def blur_address(address):
--- TaoPai/tpcommon/trans.py
+++ TaoPai/tpcommon/trans.py
@@ -1,5 +1,10 @@
 # coding: utf-8
-from os import times
+import os, sys
+# add python path of tpcommon's parent path to sys.path
+parent_path = os.path.abspath(os.path.join(__file__, *([".."]*2)))
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+
 from conflux import (
     Conflux,
     HTTPProvider
