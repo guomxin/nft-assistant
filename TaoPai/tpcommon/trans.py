0fc54a62170f14791fe1220190bf17bdfdb605bf
guomxin <guomaoxin@foxmail.com>
2022-06-23 09:09:01 +0800

    每小时的交易分析功能输出时tags去重

--- TaoPai/tpcommon/trans.py
+++ TaoPai/tpcommon/trans.py
@@ -67,57 +67,6 @@ def get_transinfo_from_data(trans_data, contra):
 
     return (from_addr, to_addr, token_id)
 
-def analyze_transaction_logs(trans_file_name, contract_addr, contract_ABI, start_date_str, end_date_str, result_file_name, min_tid=-1, max_tid=-1, verbose=True):
-    provider = HTTPProvider('https://main.confluxrpc.com')
-    c =  Conflux(provider)
-    contra = c.contract(contract_addr, contract_ABI)
-    date2tradeinfo = {} #date(%Y/%m/%d): (trade_cnt, open_mbox_cnt)
-    start_date = datetime.datetime.strptime(start_date_str + " 0:0", "%Y/%m/%d %H:%M")
-    end_date = datetime.datetime.strptime(end_date_str + " 23:59", "%Y/%m/%d %H:%M")
-    
-    with open(trans_file_name) as f:
-        rows = csv.reader(f)
-        target_row_cnt = 0
-        is_first_row = True
-        for row in rows:
-            if is_first_row:
-                is_first_row = False
-                continue
-            trans_hash = row[TransHashIndex]
-            trans_date_str = row[TransDateIndex]
-            trans_date = datetime.datetime.strptime(trans_date_str, "%Y/%m/%d %H:%M:%S")
-            trans_date_short_str = trans_date.strftime("%Y/%m/%d")
-            if (trans_date < start_date) or (trans_date > end_date):
-                continue
-            if trans_date_short_str not in date2tradeinfo:
-                date2tradeinfo[trans_date_short_str] = [0, 0]
-            trans_data = c.cfx.getTransactionByHash(trans_hash)
-            (from_addr, to_addr, token_id) = get_transinfo_from_data(trans_data, contra)
-            if (min_tid != -1) and token_id < min_tid:
-                continue
-            if (max_tid != -1) and token_id > max_tid:
-                continue
-            #print(trans_hash, trans_date_short_str)
-            if to_addr == Taopai_Conflux_Address:
-                # to address 如果是淘派的地址，说明是合成行为，不计入统计
-                continue
-            if from_addr == Taopai_Conflux_Address:
-                # from address 如果是淘派的地址，说明是打开盲盒或者空投
-                date2tradeinfo[trans_date_short_str][1] += 1
-            else:
-                # 持有者之间的交易
-                date2tradeinfo[trans_date_short_str][0] += 1
-            target_row_cnt += 1
-    
-    if verbose:
-        print("{} target transaction records found.".format(target_row_cnt))
-
-    with open(result_file_name, "w", encoding="utf-8-sig") as result_file:
-        for (td_str, (trade_cnt, open_mbox_cnt)) in date2tradeinfo.items():
-            result_file.write("{},{},{}\n".format(
-                td_str, trade_cnt, open_mbox_cnt
-            ))
-
 def get_tag_from_tokenid(token_id, ranges, tags):
     for tag_index in range(len(tags)):
         min_tid, max_tid = ranges[tag_index]
@@ -218,6 +167,13 @@ def multi_analyze_transaction_logs(trans_file_name, tradeprice_dict, contract_ad
     
     if verbose:
         print("{} target transaction records found.".format(target_row_cnt))
+
+    # 由于tags指定时可以重复，对应不同的id区间（见佛系熊猫二期的常规款），输出时tags需要去重
+    nodup_tags = []
+    for tag in tags:
+        if tag not in nodup_tags:
+            nodup_tags.append(tag)
+    tags = nodup_tags
 
     with open(result_file_name, "w", encoding="utf-8-sig") as result_file:
         # 写入交易的数量
@@ -480,8 +436,8 @@ def multi_analyze_transaction_logs_hourly_online(contract_addr, contract_ABI, da
             if not tag:
                 continue
             if to_addr == Taopai_Conflux_Address:
-                # to address 如果是淘派的地址，说明是合成行为，不计入统计
-                #details.write("{},{},{},合成\n".format(from_addr, to_addr, trans_date_str))
+                # to address 如果是淘派的地址，说明是合成或回收行为，不计入统计
+                details.write("{},{},{},合成或回收,{},{}\n".format(from_addr, to_addr, trans_time_str, tag, token_id))
                 continue
             if from_addr == Taopai_Conflux_Address:
                 # from address 如果是淘派的地址，说明是打开盲盒或者空投
