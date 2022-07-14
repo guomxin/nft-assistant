7e979f1e1eee53f5be3875e927f4298b17df735a
guomxin <guomaoxin@foxmail.com>
2022-07-14 18:40:22 +0800

    加入绿码

--- TaoPai/scripts/cron_weekly_run.sh
+++ TaoPai/scripts/cron_weekly_run.sh
@@ -28,8 +28,8 @@ python transaction_conflux_contract_online.py kaozaifriends2 $oneweekago $yester
 
 echo "---勋章---"
 python transaction_conflux_contract_online.py xunzhang $oneweekago $yesterday \
-60001,70000\;201,218\;219,303\;2501,2705\;70001,82000\;1,200\;4001,4200 \
-ZAONIAO\;DASHI\;JINGYING\;CESHIDASHI\;QINGLIANG\;WANGZHE\;ZHIYUANZHE $tag
+60001,70000\;201,218\;219,303\;2501,2705\;70001,82000\;1,200\;4001,4200\;1101,1700 \
+ZAONIAO\;DASHI\;JINGYING\;CESHIDASHI\;QINGLIANG\;WANGZHE\;ZHIYUANZHE\;LVMA $tag
 
 echo "---2022光符---"
 python transaction_conflux_contract_online.py taopai2022 $oneweekago $yesterday 30001,32022 ANY $tag
