34ba7c9f7267c62ba0ee1d0dd9eb3158c2f80384
guomxin <guomaoxin@foxmail.com>
2022-08-29 10:14:14 +0800

    添加熊熊神像的分析

--- TaoPai/scripts/dig_detail-trans_baibianxiong.sh
+++ TaoPai/scripts/dig_detail-trans_baibianxiong.sh
@@ -21,6 +21,7 @@ python diginfo_from_details_conflux.py baibianxiong 1 data/_details_conflux_baib
 # python diginfo_from_details_conflux.py baibianxiong 2 data/_details_conflux_baibianxiong_result_$1.csv 3001,10000 18 N18 $1
 # python diginfo_from_details_conflux.py baibianxiong 2 data/_details_conflux_baibianxiong_result_$1.csv 501,3000 6 R6 $1
 # python diginfo_from_details_conflux.py baibianxiong 2 data/_details_conflux_baibianxiong_result_$1.csv 501,3000\;3001,10000 3\;6 FRAGMENT $1
+python diginfo_from_details_conflux.py baibianxiong 2 data/_details_conflux_baibianxiong_result_$1.csv 18001,18437\;18438,18777\;18778,18961 1\;1\;1 HECHENG-ZUANSHISX $1
 
 # 4. dig count in circulation
 python diginfo_from_details_conflux.py baibianxiong 3 data/_details_conflux_baibianxiong_result_$1.csv $1
