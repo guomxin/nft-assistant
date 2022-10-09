4e469cf3a0e06a104c3b9be83230581ab6672e1d
guomxin <guomaoxin@foxmail.com>
2022-10-09 17:10:50 +0800

    init脚本

---
+++ GuangYu/scripts/init.sh
@@ -0,0 +1,8 @@
+cd ..
+
+python grab_transaction_price.py 54 20221009
+python grab_transaction_price.py 59 20221009
+python grab_transaction_price.py 39 20221009
+python grab_transaction_price.py 56 20221009
+
+cd scripts
