1cfcf798266506a5d3cb66af6c6e2383e0d84bca
guomxin <guomaoxin@foxmail.com>
2022-10-21 16:53:07 +0800

    分析成交价格区间

---
+++ GuangYu/analyze_transaction_prices.py
@@ -0,0 +1,73 @@
+# coding: utf-8
+
+import sys
+import datetime
+
+from gycommon import commoninfo
+
+DETAIL_SELLER_ID_INDEX = 0
+DETAIL_SELLER_INDEX = 1
+DETAIL_BUYER_INDEX = 2
+DETAIL_SALE_TIME_INDEX = 3
+DETAIL_PRICE_INDEX = 4
+DETAIL_TOKEN_ID_INDEX = 5
+DETAIL_PROD_ID_INDEX = 6
+DETAIL_DETAIL_ID_INDEX = 7
+DETAIL_ITEM_COUNT = 8
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("{} <casting_id> <tag> <bucket_size>".format(sys.argv[0]))
+        sys.exit(1)
+    casting_id = int(sys.argv[1])
+    if casting_id not in commoninfo.CastingId2MetaInfo:
+        print("CastingId:{} not exist!")
+        sys.exit(1)
+    casting_name = commoninfo.CastingId2MetaInfo[casting_id][0]
+    tag = sys.argv[2]
+    bucket_size = int(sys.argv[3])
+
+    token2price = {}
+    trans_logs_file_name = "data/" + commoninfo.Transaction_Logs_File_Name.format(casting_name)
+    with open(trans_logs_file_name, encoding="utf-8-sig") as trans_logs_file:
+        # 加载已有的交易信息
+        for line in trans_logs_file:
+            items = line.strip().split(",")
+            #seller_id = int(items[DETAIL_SELLER_ID_INDEX])
+            #seller_name = items[DETAIL_SELLER_INDEX]
+            sale_time = datetime.datetime.strptime(
+                items[DETAIL_SALE_TIME_INDEX], "%Y/%m/%d %H:%M:%S")
+            price = float(items[DETAIL_PRICE_INDEX])
+            token_id = int(items[DETAIL_TOKEN_ID_INDEX])
+            if token_id not in token2price:
+                token2price[token_id] = [price, sale_time]
+            else:
+                if sale_time > token2price[token_id][1]:
+                    token2price[token_id] = [price, sale_time]
+    
+    # 按成交价格倒序排列
+    token_price_info = []
+    for (token_id, (price, _)) in token2price.items():
+        token_price_info.append([token_id, price])
+    token_price_info.sort(key=lambda i: i[1], reverse=True)
+    token_cnt = len(token_price_info)
+    if token_cnt == 0:
+        sys.exit(0)
+    highest_price = token_price_info[0][1]
+    bucket_cnt = int(highest_price) // bucket_size + 1
+    buckets = [0] * bucket_cnt
+    for (_, price) in token_price_info:
+        bucket_index = int(price) // bucket_size
+        #for i in range(bucket_index+1):
+        #    buckets[i] += 1
+        buckets[bucket_index] += 1
+    result_file_name = "data/_analyze_trans_price_result_{}_{}.csv".format(casting_name, tag)
+    with open(result_file_name, "w", encoding="utf-8-sig") as result_file:
+        result_file.write("总数量:{}\n".format(token_cnt))
+        result_file.write("价格区间,数量,占比,累积\n")
+        for i in range(len(buckets)):
+            result_file.write("{},{},{:.2%},{:.2%}\n".format(
+                "[{}-{})".format(i * bucket_size, (i + 1) * bucket_size),
+                buckets[i], buckets[i] / token_cnt,
+                sum(buckets[:i+1]) / token_cnt
+            ))
--- GuangYu/scripts/cron_run.sh
+++ GuangYu/scripts/cron_run.sh
@@ -25,81 +25,107 @@ python scan_circulation_info.py $today
 
 echo "---启源I开拓者号---"
 python grab_transaction_price.py 54 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 54 ${yesterday} 100
 
 echo "---万象龙巢---"
 python grab_transaction_price.py 59 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 59 ${yesterday} 100
 
 echo "---空间补给站-启能版---"
 python grab_transaction_price.py 39 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 39 ${yesterday} 50
 
 echo "---云木守护版---"
 python grab_transaction_price.py 57 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 57 ${yesterday} 50
 
 echo "---小小键盘-Ctrl仔---"
 python grab_transaction_price.py 56 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 56 ${yesterday} 50
 
 echo "---小小键盘-C仔---"
 python grab_transaction_price.py 67 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 67 ${yesterday} 50
 
 echo "---小小键盘-V仔---"
 python grab_transaction_price.py 66 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 66 ${yesterday} 50
 
 echo "---小小键盘-Shift仔---"
 python grab_transaction_price.py 60 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 60 ${yesterday} 50
 
 echo "---传说奇遇-太空Shift仔---"
 python grab_transaction_price.py 61 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 61 ${yesterday} 50
 
 echo "---山丘龙灵-厚土---"
 python grab_transaction_price.py 31 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 31 ${yesterday} 50
 
 echo "---河流龙灵-甘霖---"
 python grab_transaction_price.py 32 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 32 ${yesterday} 50
 
 echo "---焰火龙灵-灰烬---"
 python grab_transaction_price.py 33 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 33 ${yesterday} 100
 
 echo "---金晶龙灵-浮金---"
 python grab_transaction_price.py 30 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 30 ${yesterday} 100
 
 echo "---国庆节限定空投-龙凤筷---"
 python grab_transaction_price.py 75 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 75 ${yesterday} 50
 
 echo "---龙图腾---"
 python grab_transaction_price.py 79 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 79 ${yesterday} 50
 
 echo "---龙凤守宝---"
 python grab_transaction_price.py 80 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 80 ${yesterday} 50
 
 echo "---探索者-Ctrl---"
 python grab_transaction_price.py 83 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 83 ${yesterday} 50
 
 echo "---探索者-Shift---"
 python grab_transaction_price.py 84 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 84 ${yesterday} 50
 
 echo "---物资传输面板---"
 python grab_transaction_price.py 28 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 28 ${yesterday} 50
 
 echo "---栖龙云木---"
 python grab_transaction_price.py 29 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 29 ${yesterday} 50
 
 echo "---梦幻小龙---"
 python grab_transaction_price.py 46 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 46 ${yesterday} 50
 
 echo "---山岭树龙---"
 python grab_transaction_price.py 55 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 55 ${yesterday} 50
 
 echo "---凤图腾---"
 python grab_transaction_price.py 87 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 87 ${yesterday} 50
 
 echo "---阿尔法之眼---"
 python grab_transaction_price.py 94 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 94 ${yesterday} 100
 
 echo "---魂魄提灯---"
 python grab_transaction_price.py 95 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 95 ${yesterday} 50
 
 echo "---涅槃之地---"
 python grab_transaction_price.py 96 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 96 ${yesterday} 100
 
 #------ 后处理 ------#
 cd data;mkdir -p upload/$yesterday;rm -rf upload/$yesterday/*
