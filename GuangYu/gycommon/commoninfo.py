1b4a49b66c52880763f6062aa6205fb48466dc3e
guomxin <guomaoxin@foxmail.com>
2022-11-08 22:32:30 +0800

    最高限价变为50000

--- GuangYu/gycommon/commoninfo.py
+++ GuangYu/gycommon/commoninfo.py
@@ -56,6 +56,6 @@ CastingId2MetaInfo = {
 Transaction_Logs_File_Name = "_guangyu_{}_trans_logs.csv"
 
 HighestValue_Products_Info = {
-    80: [258, 45000.0],
-    30: [88, 45000.0],
+    80: [258, 50000.0],
+    30: [88, 50000.0],
 }
