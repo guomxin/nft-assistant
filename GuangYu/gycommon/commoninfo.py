9f87f84d59cfe4230d5f73ddad5203d811016394
guomxin <guomaoxin@foxmail.com>
2022-11-13 13:32:49 +0800

    更新退市价格

--- GuangYu/gycommon/commoninfo.py
+++ GuangYu/gycommon/commoninfo.py
@@ -63,6 +63,6 @@ CastingId2MetaInfo = {
 Transaction_Logs_File_Name = "_guangyu_{}_trans_logs.csv"
 
 HighestValue_Products_Info = {
-    80: [258, 50000.0],
-    30: [88, 50000.0],
+    80: [258, 55000.0],
+    30: [88, 55000.0],
 }
