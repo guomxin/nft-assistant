af70dfe882615fc8774b7985c1fa67522dac8428
guomxin <guomaoxin@foxmail.com>
2022-12-31 11:37:50 +0800

    处理两种文件

--- GuangYu/docx2encryptpdf.py
+++ GuangYu/docx2encryptpdf.py
@@ -2,18 +2,7 @@ import sys
 from docx2pdf import convert
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("{} <tag>".format(sys.argv[0]))
-        sys.exit(1)
-    tag = sys.argv[1]
-    docx_file_name = "data/日交易额_{}.docx".format(
-        tag
-    )
-    pdf_file_name = "data/日交易额_{}.pdf".format(
-        tag
-    ) 
-    
+def convert_file(docx_file_name, pdf_file_name):
     convert(docx_file_name, pdf_file_name)
     # 禁掉pdf的各种权限（包括复制）
     pdf_reader = PdfFileReader(pdf_file_name)
@@ -27,3 +16,27 @@ if __name__ == "__main__":
     pdf_writer.encrypt("", "shuang", permissions_flag = 0)
     with open(pdf_file_name, "wb") as out:
         pdf_writer.write(out)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("{} <tag>".format(sys.argv[0]))
+        sys.exit(1)
+    tag = sys.argv[1]
+
+    docx_file_name = "data/日交易额_{}_按交易额排序.docx".format(
+        tag
+    )
+    pdf_file_name = "data/日交易额_{}_按交易额排序.pdf".format(
+        tag
+    ) 
+    convert_file(docx_file_name, pdf_file_name)
+
+    docx_file_name = "data/日交易额_{}_按交易量排序.docx".format(
+        tag
+    )
+    pdf_file_name = "data/日交易额_{}_按交易量排序.pdf".format(
+        tag
+    ) 
+    convert_file(docx_file_name, pdf_file_name)
+    
+    
