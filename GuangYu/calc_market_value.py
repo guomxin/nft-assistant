711fd8f6519a6b35cf757bcff9adea105a6cf463
guomxin <guomaoxin@foxmail.com>
2023-01-07 10:16:14 +0800

    计算4,6,11th市值

--- GuangYu/calc_market_value.py
+++ GuangYu/calc_market_value.py
@@ -73,11 +73,14 @@ if __name__ == "__main__":
     casting2value = {}
     casting2holding = {}
     total_value = 0
+    total_fourth_value = 0
+    total_sixth_value = 0
+    total_11th_value = 0
     my_share_value = 0
     for casting_id in commoninfo.CastingId2MetaInfo:
-        if casting_id == 87 or casting_id == 75:
-            # 忽略凤图腾和龙凤筷
-            continue
+        #if casting_id == 87 or casting_id == 75:
+        #    # 忽略凤图腾和龙凤筷
+        #    continue
         time.sleep(2)
         casting_name = commoninfo.CastingId2MetaInfo[casting_id][1]
         print("获取[{}]的信息...".format(casting_name))
@@ -95,8 +98,23 @@ if __name__ == "__main__":
                 # 按已退市，按最高限额计算
                 print("\t无在售信息, casting={}".format(casting_name))
                 min_price = commoninfo.HighestValue_Products_Info[casting_id][1]
+                fourth_price = commoninfo.HighestValue_Products_Info[casting_id][1]
+                sixth_price = commoninfo.HighestValue_Products_Info[casting_id][1]
+                eleth_price = commoninfo.HighestValue_Products_Info[casting_id][1]
             else:
                 min_price = saling_prods[0][PRICE_INDEX]
+                if len(saling_prods) > 3:
+                    fourth_price = saling_prods[3][PRICE_INDEX]
+                else:
+                    fourth_price = saling_prods[-1][PRICE_INDEX]
+                if len(saling_prods) > 5:
+                    sixth_price = saling_prods[5][PRICE_INDEX]
+                else:
+                    sixth_price = saling_prods[-1][PRICE_INDEX]
+                if len(saling_prods) > 10:
+                    eleth_price = saling_prods[10][PRICE_INDEX]
+                else:
+                    eleth_price = saling_prods[-1][PRICE_INDEX]
         else:
             # 已退市或关闭寄售
             if casting_id in commoninfo.HighestValue_Products_Info:
@@ -104,6 +122,9 @@ if __name__ == "__main__":
                 print("\t已退市, casting={}".format(casting_name))
                 circu_cnt = commoninfo.HighestValue_Products_Info[casting_id][0]
                 min_price = commoninfo.HighestValue_Products_Info[casting_id][1]
+                fourth_price = commoninfo.HighestValue_Products_Info[casting_id][1]
+                sixth_price = commoninfo.HighestValue_Products_Info[casting_id][1]
+                eleth_price = commoninfo.HighestValue_Products_Info[casting_id][1]
             else:
                 # 已关闭寄售，暂不处理
                 print("\t已关闭寄售, casting={}".format(casting_name))
@@ -114,6 +135,9 @@ if __name__ == "__main__":
         if my_holding_cnt > 0:
             casting2holding[casting_name] = [my_holding_cnt, min_price, min_price * my_holding_cnt]
         total_value += circu_cnt * min_price
+        total_fourth_value += circu_cnt * fourth_price
+        total_sixth_value += circu_cnt * sixth_price
+        total_11th_value += circu_cnt * eleth_price
         my_share_value += my_holding_cnt * min_price
     
     casting_value_infos = []
@@ -150,13 +174,16 @@ if __name__ == "__main__":
         content += "{}\n>持仓量:{}\n>当前最低价:{}\n>当前价值:{:.2f}万\n\n".format(
             casting_name, holding_cnt, 
             min_price, hvalue / 10000)
-    utils.send_workwx_msg_agg(utils.HoldingShare_MSG, "markdown", content)
+    #utils.send_workwx_msg_agg(utils.HoldingShare_MSG, "markdown", content)
 
     result_file_name = "data/_calc_market_value_{}.csv".format(
         tag
     )
     with open(result_file_name, "w", encoding="utf-8-sig") as result_file:
         result_file.write("总市值: {:.2f}万\n".format(total_value / 10000))
+        result_file.write("总市值(4th): {:.2f}万\n".format(total_fourth_value / 10000))
+        result_file.write("总市值(6th): {:.2f}万\n".format(total_sixth_value / 10000))
+        result_file.write("总市值(11th): {:.2f}万\n".format(total_11th_value / 10000))
         result_file.write("{},{},{},{}\n".format(
             "名称", "流通量", "挂牌最低价", "市值(万)"
         ))
