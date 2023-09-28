61efa8c2e09ee9f9f51a499f217d8ce034cc0f0e
guomxin <guomaoxin@foxmail.com>
2023-09-28 18:25:58 +0800

    增加推荐区产品统计

--- GuangYu/scripts/cron_run.sh
+++ GuangYu/scripts/cron_run.sh
@@ -99,6 +99,22 @@ echo "---小小键盘-K仔---"
 python grab_transaction_price.py 3919 ${yesterday} ${yesterday}
 python analyze_transaction_prices.py 3919 ${yesterday} 20
 
+echo "---解构光束---"
+python grab_transaction_price.py 3818 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 3818 ${yesterday} 10
+
+echo "---超速传动装置---"
+python grab_transaction_price.py 3733 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 3733 ${yesterday} 50
+
+echo "---未解序桩---"
+python grab_transaction_price.py 3843 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 3843 ${yesterday} 50
+
+echo "---地核熔岩---"
+python grab_transaction_price.py 3851 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 3851 ${yesterday} 100
+
 #------ 后处理 ------#
 cd data;mkdir -p upload/$yesterday;rm -rf upload/$yesterday/*
 cp *$yesterday* upload/$yesterday;cd upload/$yesterday
@@ -107,7 +123,7 @@ for nft in "1-N-QingKongYuFengZhou" "1-N-MengYouGeLou" "1-N-ChaoNengMoJing" \
 "1-SR-YingMuHuanShaPeng" "1-SR-QuanJiShouTao" "1-SSR-MoDengZhongXin" "1-SSR-ZhongGuiTieDao" "1-SSR-ChaoShanDianCi" \
 "2-UR-WeiLaiDuShi" "2-UR-YuanYangDuLun" "2-UR-ShiKongChuanSuoJi" "2-UR-YueQiuShangCheng" \
 "2-ZaiFuXiangHuang" "2-LongYinWanXiang" "2-HuanShuShi" "2-MoShuShi" "3-JiXingGuangLun" "3-BingHuangGuiZuo" \
-"3-XiaoXiaoJianPan-KZai"
+"3-XiaoXiaoJianPan-KZai" "4-JieGouGuangShu" "4-ChaoSuChuanDongZhuangZhi" "4-WeiJieXuZhuang" "4-DiHeRongYan"
 do
     mkdir $nft;mv *$nft*.csv $nft;zip -q $nft-$yesterday $nft/*
 done
