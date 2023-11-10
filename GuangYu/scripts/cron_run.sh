c99e4dbe4a61b2904afbdd599429a5eac08e8428
guomxin <guomaoxin@foxmail.com>
2023-11-10 08:49:43 +0800

    更新推荐区

--- GuangYu/scripts/cron_run.sh
+++ GuangYu/scripts/cron_run.sh
@@ -131,9 +131,6 @@ python grab_transaction_price.py 3981 ${yesterday} ${yesterday}
 python analyze_transaction_prices.py 3981 ${yesterday} 100
 
 #--- 推荐区 ---#
-echo "---小小键盘-K仔---"
-python grab_transaction_price.py 3919 ${yesterday} ${yesterday}
-python analyze_transaction_prices.py 3919 ${yesterday} 20
 
 echo "---解构光束---"
 python grab_transaction_price.py 3818 ${yesterday} ${yesterday}
@@ -163,7 +160,7 @@ for nft in "1-N-QingKongYuFengZhou" "1-N-MengYouGeLou" "1-N-ChaoNengMoJing" \
 "1-SR-YingMuHuanShaPeng" "1-SR-QuanJiShouTao" "1-SSR-MoDengZhongXin" "1-SSR-ZhongGuiTieDao" "1-SSR-ChaoShanDianCi" \
 "2-UR-WeiLaiDuShi" "2-UR-YuanYangDuLun" "2-UR-ShiKongChuanSuoJi" "2-UR-YueQiuShangCheng" \
 "2-ZaiFuXiangHuang" "2-LongYinWanXiang" "2-HuanShuShi" "2-MoShuShi" "2-JinQuXiYin" "3-ZhongYiWuSheng" "3-ChuFengMingTong" \
-"3-XiaoXiaoJianPan-KZai"  "3-HuBenZhanXue" "3-YinXuJianDao" "3-FengDengBaiGu" "3-YongMengShuBing" "3-KuaiHuRen" "3-RuiFengJianLing" \
+"3-HuBenZhanXue" "3-YinXuJianDao" "3-FengDengBaiGu" "3-YongMengShuBing" "3-KuaiHuRen" "3-RuiFengJianLing" \
 "3-YunYuYangFeng" "4-JieGouGuangShu"  "4-FeiHongZhiShan" "5-PanYanShengSuo" "5-XueYuZhiDu" "5-QiongYuFeiLun"
 do
     mkdir $nft;mv *$nft*.csv $nft;zip -q $nft-$yesterday $nft/*
