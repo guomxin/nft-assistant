62219e3cfebd64a7e4168d3bac1e55eca2f2fbfd
guomxin <guomaoxin@foxmail.com>
2024-04-28 10:44:22 +0800

    更新统计

--- GuangYu/gycommon/commoninfo.py
+++ GuangYu/gycommon/commoninfo.py
@@ -528,6 +528,7 @@ CastingId2MetaInfo = {
     5290: ["5-ShuiHu-LiJun", "水浒-李俊", 5],
     5314: ["5-ChaoTing-LiGang", "朝廷-李纲", 5],
     5326: ["5-JinXing", "金星", 10],
+    5319: ["5-ZhaoYeYuShiZi", "照夜玉狮子", 5],
 
     #4443: ["6-XuShiZhongShu", "虚实中枢"],
     #4448: ["6-XuNiLiRen", "虚拟利刃"],
