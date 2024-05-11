36782a6c480e698c58e617e565c5b87031aea952
guomxin <guomaoxin@foxmail.com>
2024-05-11 09:41:09 +0800

    更新统计

--- GuangYu/gycommon/commoninfo.py
+++ GuangYu/gycommon/commoninfo.py
@@ -526,8 +526,8 @@ CastingId2MetaInfo = {
     5321: ["5-RanLiao", "燃料", 2],
     5312: ["5-ChaoTing-HanCunBao", "朝廷-韩存保", 5],
     5290: ["5-ShuiHu-LiJun", "水浒-李俊", 5],
-    5314: ["5-ChaoTing-LiGang", "朝廷-李纲", 5],
-    5326: ["5-JinXing", "金星", 10],
+    5314: ["5-ChaoTing-LiGang", "朝廷-李纲", 5], #------
+    5326: ["5-JinXing", "金星", 10], #------
     5319: ["5-ZhaoYeYuShiZi", "照夜玉狮子", 5],
     5330: ["5-YunShi", "陨石", 5],
     #5289: ["5-ShuiHu-XiaoRang", "水浒-萧让", 5], #------
@@ -537,9 +537,11 @@ CastingId2MetaInfo = {
     5325: ["5-SiKuQuanShu-SongShi", "四库全书-宋史", 5],
     5338: ["5-HuoXing", "火星", 5],
     5337: ["5-JinChao-WanYanAGuDa", "金朝-完颜阿骨打", 5],
-    5341: ["5-ShuiXing", "水星", 5],
+    5341: ["5-ShuiXing", "水星", 5], #------
     5320: ["5-ChaoTing-WuJie", "朝廷-吴玠", 5],
     5350: ["5-HuiXing", "慧星", 5],
+    5354: ["5-YuanHangFeiChuan", "远航飞船", 5],
+    5355: ["5-YueQiu", "月球", 5],
 
     #4443: ["6-XuShiZhongShu", "虚实中枢"],
     #4448: ["6-XuNiLiRen", "虚拟利刃"],
@@ -792,7 +794,7 @@ HighestValue_Products_Info = {
 }
 
 # 131
-Query_Token= "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMzE1NjE2MTA5MSIsInNvdXJjZSI6InBjIiwidHlwZSI6ImN1c3RvbWVyIiwiZXhwIjoxNzE1MzMxNTMzLCJzaWduSWQiOiI0ZjBhNGFjMWRmYjA0NGQwODQxM2VmNDk4N2VhOGIyNiIsImlhdCI6MTcxNDcyNjczM30.ZpZQR_siOSkc0FY2xSQbbhAUpeg0GOWhZhu9DLep0Ic"
+Query_Token= "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMzE1NjE2MTA5MSIsInNvdXJjZSI6InBjIiwidHlwZSI6ImN1c3RvbWVyIiwiZXhwIjoxNzE1OTk1ODExLCJzaWduSWQiOiI5MDYyZjlmNWU4ZGQ0MzQ5ODBmMTY3ZDg0ZDBjM2EyYiIsImlhdCI6MTcxNTM5MTAxMX0.Of1QegSXkrgMeEdcGSduHJr99ZV_uIV3IQ94ujCN0Y4"
 
 # Pay
 Home_Token = Query_Token
--- GuangYu/scripts/cron_run.sh
+++ GuangYu/scripts/cron_run.sh
@@ -298,6 +298,14 @@ echo "---慧星---"
 python grab_transaction_price.py 5350 ${yesterday} ${yesterday}
 python analyze_transaction_prices.py 5350 ${yesterday} 10
 
+echo "---远航飞船---"
+python grab_transaction_price.py 5354 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 5354 ${yesterday} 10
+
+echo "---月球---"
+python grab_transaction_price.py 5355 ${yesterday} ${yesterday}
+python analyze_transaction_prices.py 5355 ${yesterday} 10
+
 #--- 赛博狂潮 ---#
 echo "---赛博起飞卡---"
 python grab_transaction_price.py 4985 ${yesterday} ${yesterday}
@@ -480,7 +488,7 @@ for nft in "1-N-MengYouGeLou" "1-N-YouLingTianTai" "1-N-ChaoNengMoJing" \
 "5-ShuiHu-SuoChao" "5-TanSuoFeiChuan" "5-RanLiao" "5-ChaoTing-HanCunBao" "5-ZengNong" \
 "5-WangLun" "5-ShuiHu-LuJunYi" "5-ShuiHu-DuanJingZhu" "5-ShenMiSuiDaoTongXingPingZheng" \
 "5-ShuiHu-DaiZong" "5-SiKuQuanShu-SongShi" "5-HuoXing" "5-JinChao-WanYanAGuDa" "5-ShuiXing" \
-"5-ChaoTing-WuJie" "5-HuiXing" \
+"5-ChaoTing-WuJie" "5-HuiXing" "5-YuanHangFeiChuan" "5-YueQiu" \
 "6-SaiBoQiFeiKa" "6-EMoXunZhang"  "6-SaiBoNiYu" "6-SaiBoTopOne" "6-SaiBoTop" "6-SaiBoPoYiHeXin" \
 "6-SaiBoJiangZiYa" "6-SaiBoKawasaki" \
 "7-XiangSuQiJing" "7-TanXianZheIV" "7-XunBaoZheI" "7-GongYunXiaoShao" "7-FangBiaoZhiShiDeng" "7-YeGuWangCheng" \
